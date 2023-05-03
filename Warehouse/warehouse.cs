2251b5e22f5c56b8b622da4d2906d00fc2fbce41
vafle <vafle@DESKTOP-D6239CC>
2023-05-03 12:26:04 +0300

    KindaDone

--- Warehouse/warehouse.cs
+++ Warehouse/warehouse.cs
@@ -16,7 +16,7 @@ namespace Warehouse
 {
     public partial class warehouse : Form
     {
-        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\AppData\WarehouseDB (1).mdf;Integrated Security=True");
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\vafle\source\repos\Warehouse\Warehouse\AppData\WarehouseDB (1).mdf;Integrated Security=True");
         SqlCommand cmd, cmdcheckId, cmdcheckCat_Name, cmdCheck;
         SqlDataAdapter adapter_toode, adapter_kat;
         OpenFileDialog piltValiDialog;
