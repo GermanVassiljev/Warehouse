83437d2f5334df81566fded6dd2027ab4658d502
opilane <opilane@HP-CZC1339D1Q>
2023-05-19 10:14:05 +0300

    Database was fixed

--- Warehouse/warehouse.cs
+++ Warehouse/warehouse.cs
@@ -17,7 +17,7 @@ namespace Warehouse
     public partial class warehouse : Form
     {
         string connectPath = System.Environment.CurrentDirectory+ "\\AppData\\WarehouseDB_1.mdf";
-        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\opilane\source\repos\Warehouse\Warehouse\AppData\WarehouseDB_1.mdf;Integrated Security=True");
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\AppData\WarehouseDB_1.mdf;Integrated Security=True");
         SqlCommand cmd, cmdcheckId, cmdcheckCat_Name, cmdCheck;
         SqlDataAdapter adapter_toode, adapter_kat;
         OpenFileDialog piltValiDialog;
